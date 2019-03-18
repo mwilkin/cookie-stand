79f9779f4454061f1f09672a850192808a238d51
Matt Wilkin <mwilkin22@gmail.com>
2019-03-18 15:33:22 -0700

    add simple styling.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,43 @@
 'use strict';
+
+var oldMill = {
+  name: 'Old Mill',
+  minHourlyCustomers: 23,
+  maxHourlyCustomers: 65, 
+  averageCookieSale: 6.3,
+  shout: function(){
+    console.log(this.name);
+  }
+};
+
+
+
+
+// var drakePark = {
+//   name: 'Drake Park',
+//   minHourlyCustomers: 3,
+//   maxHourlyCustomers: 24, 
+//   averageCookieSale: 1.2,
+// };
+
+// var pilotButte = {
+//   name: 'Pilot Butte',
+//   minHourlyCustomers: 11,
+//   maxHourlyCustomers: 38, 
+//   averageCookieSale: 3.7,
+// };
+
+// var lesSchwabAmphitheater = {
+//   name: 'Les Schwab Amphitheater',
+//   minHourlyCustomers: 20,
+//   maxHourlyCustomers: 38, 
+//   averageCookieSale: 2.3,
+// };
+
+// var towerTheater = {
+//   name: 'Tower Theater',
+//   minHourlyCustomers: 2,
+//   maxHourlyCustomers: 16, 
+//   averageCookieSale: 4.6,
+// };
+
