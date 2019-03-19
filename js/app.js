ec1816052928d651e4aa16e39b0de11c8713487d
Matt Wilkin <mwilkin22@gmail.com>
2019-03-19 16:07:52 -0700

    establish a working random customer function and array to store results, additionally established a working cookies sold hourly function and array to store results.

--- js/app.js
+++ js/app.js
@@ -4,31 +4,73 @@ var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '
 
 // ToDo: make sure to include Total after the list of hours.
 
-var customers = [];
+// var cookiesByHour = [];
+
+// totalCookiesSold =+ cookiesByHour;
 
 var oldMill = {
   name: 'Old Mill',
   minHourlyCustomers: 23,
   maxHourlyCustomers: 65, 
   averageCookieSale: 6.3,
-  randomCustomerResult: 0,
-  cookiesSoldHourly: 0,
+  randomCustomerResult: [],
+  cookiesSoldHourly: [],
+  cookiesByHour: [],
+  // totalCookiesSold: 0,
+  // Math.round(this.averageCookieSale * this.randomCustomerResult[i]);
   calculateRandomNumberOfCustomers: function() {
-    var min = Math.ceil(this.minHourlyCustomers);
-    var max = Math.floor(this.maxHourlyCustomers);
-    this.randomCustomerResult =  Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers + 1)) + this.minHourlyCustomers;
+    for(var i = 0; i < hours.length; i++){
+      this.randomCustomerResult.push(Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers + 1)) + this.minHourlyCustomers);
+    }
   },
   calculateCookiesSoldHourly: function() {
-    this.cookiesSoldHourly = Math.round(this.averageCookieSale * this.randomCustomerResult);
-  }
+    this.calculateRandomNumberOfCustomers();
+    for(var i = 0; i < hours.length; i++ ) {
+      this.cookiesSoldHourly.push(Math.round(this.randomCustomerResult[i] * this.averageCookieSale));
+    }
+  },
 };
 
 console.log(oldMill.minHourlyCustomers);
 console.log(oldMill.maxHourlyCustomers);
-oldMill.calculateRandomNumberOfCustomers();
+// oldMill.calculateRandomNumberOfCustomers();
 console.log(oldMill.randomCustomerResult);
 oldMill.calculateCookiesSoldHourly();
 console.log(oldMill.cookiesSoldHourly);
+console.log(oldMill.cookiesByHour); 
+
+
+// <article id="oldMill">
+//  <h2>Old Mill District</h2>
+//  <ul>
+//    <li>6am: 16 cookies</li>
+//    <li>7am: 20 cookies</li>
+//  </ul> 
+// </article>
+
+
+var shopLocationListElement = document.getElementById('shopLocationList');
+console.log(shopLocationListElement);
+
+var oldMillId = oldMill.name.toLowerCase().replace(' m','M')
+
+var article = document.createElement('article');
+article.setAttribute('id', `${oldMillId}`);
+shopLocationListElement.appendChild(article);
+
+var h2 = document.createElement('h2');
+h2.textContent = oldMill.name;
+article.appendChild(h2);
+
+var ul = document.createElement('ul');
+article.appendChild(ul);
+
+for(var i = 0; i < hours.length; i++){
+  var li = document.createElement('li');
+  li.textContent = `${hours[i]}: 100 cookies`;
+  ul.appendChild(li);
+  
+}
 
 
 // var pilotButte = {
@@ -57,39 +99,3 @@ console.log(oldMill.cookiesSoldHourly);
 //   minHourlyCustomers: 3,
 //   maxHourlyCustomers: 24, 
 //   averageCookieSale: 1.2,
-// };
-
-{
-  /* 
-  <article id="oldMill">
-    <h2>Old Mill District</h2>
-    <ul>
-      <li>6am: 16 cookies</li>
-      <li>7am: 20 cookies</li>
-    </ul> 
-  </article>*/
-}
-
-var shopLocationListElement = document.getElementById('shopLocationList');
-console.log(shopLocationListElement);
-
-var oldMillId = oldMill.name.toLowerCase().replace(' m','M')
-
-var article = document.createElement('article');
-article.setAttribute('id', `${oldMillId}`);
-shopLocationListElement.appendChild(article);
-
-var h2 = document.createElement('h2');
-h2.textContent = oldMill.name;
-article.appendChild(h2);
-
-var ul = document.createElement('ul');
-article.appendChild(ul);
-
-
-
-for(var i = 0; i < hours.length; i++){
-  var li = document.createElement('li');
-  li.textContent = `${hours[i]}: ${oldMill.cookiesSoldHourly} cookies`;
-  ul.appendChild(li); 
-}
