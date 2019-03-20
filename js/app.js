82499204f56244acac73bc0978fc04c99ac5affb
Matt Wilkin <mwilkin22@gmail.com>
2019-03-20 13:53:34 -0700

    first work done on lab 7, minor changes.

--- js/app.js
+++ js/app.js
@@ -6,7 +6,18 @@
 
 // RUN CODE
 
+// ---------------
+// Define all global functions
+// ---------------
+
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+// ---------------
+// Define all data to be used in script
+// ---------------
+// ---------------
+// Run script
+// ---------------
 
 // OLD MILL LOCATION
 
