51428f8861b2c85c188d18cba989d4d3862d6ace
Matt Wilkin <mwilkin22@gmail.com>
2019-03-21 16:26:55 -0700

    successfully hard coded a location and cookie totals into table.

--- js/app.js
+++ js/app.js
@@ -2,88 +2,83 @@
 
 // DEFINE ALL GLOBAL VARIABLES
 
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+var shopListLocation = [];
+
 // DEFINE ALL GLOBAL FUNCTIONS
 
 // RUN CODE
+for(var i = 0; i < shopListLocation.length; i++){
+  shopListLocation[i].render(shopLocationListElement);
+}
 
-// ---------------
-// Define all global functions
-// ---------------
+// Creating getting HTML elements
 
-var renderShop = function(parentElement){
-  var table = document.createElement('table');
-  table.setAttribute('id', `${this.name.toLowerCase().replace(' ', '_')}`);
-  parentElement.appendChild(table);
+var parentElement = document.getElementById('shopData');
 
-  var thead = document.createElement('thead');
-  parentElement.appendChild(thead);
+var table = document.createElement('table');
+parentElement.appendChild(table);
 
-  var tr = document.createElement('tr');
-  parentElement.appendChild(tr);
-  
-  var th = document.createElement('th');
-  parentElement.appendChild(th);
-  
-  for(var i = 0; i < hours.length; i++){
-    th = document.createElement('th');
-    th.textContent = hours[i]; 
-    parentElement.appendChild(th);
+var thead = document.createElement('thead');
+table.appendChild(thead);
 
+var tr = document.createElement('tr');
+thead.appendChild(tr);
+
+var th = document.createElement('th');
+tr.appendChild(th);
+
+for(var j = 0; j < hours.length; j++){
+  th = document.createElement('th');
+  th.textContent = hours[j]; 
+  tr.appendChild(th);
+}
+
+th = document.createElement('th');
+th.textContent = 'Daily Location Total';
+tr.appendChild(th); 
+// place calcuated daily total (for loop)
+
+var tbody = document.createElement('tbody');
+table.appendChild(tbody);
+
+tr = document.createElement('tr');
+tr.setAttribute('id', 'shop');
+tbody.appendChild(tr);
+
+var renderShop = function(){
+  var shopRow = document.getElementById('shop');
+  // console.log('hello in side rendershop');
+  var td = document.createElement('td');
+  td.textContent = 'Old Mill';
+  // `${this.name.toLowerCase().replace(' ', '_')}`;
+  shopRow.appendChild(td);
+
+  for(i = 0; i < hours.length; i++){
+    td = document.createElement('td');
+    td.textContent = 12;
+    // `${this.cookiesSoldHourly[i]}`
+    shopRow.appendChild(td);
   }
-
-  
-
-
-
-  var h2 = document.createElement('h2');
-  h2.textContent = this.name;
-  article.appendChild(h2);
-  
-  var ul = document.createElement('ul');
-  article.appendChild(ul);
-  
-  for(var i = 0; i < hours.length; i++){
-    var li = document.createElement('li');
-    li.textContent = `${hours[i]}: ${this.cookiesSoldHourly[i]} cookies`;
-    ul.appendChild(li);
-  }
-  
-  li = document.createElement('li');
-  li.textContent = `Total cookies sold for the day: ${this.totalCookiesSold}`;
-  ul.appendChild(li);
 };
 
+var tfoot = document.createElement('tfoot');
+table.appendChild(tfoot);
 
+tr = document.createElement('tr');
+tfoot.appendChild(tr);
 
+var td = document.createElement('td');
+td.textContent = 'Hour Totals';
+tr.appendChild(td);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//Create a for loop add all the hourly cookie sale totals
 
 // var renderShop = function(parentElement){
 //   var article = document.createElement('article');
 //   article.setAttribute('id', `${this.name.toLowerCase().replace(' ', '_')}`);
 //   parentElement.appendChild(article);
-
-//   var h2 = document.createElement('h2');
-//   h2.textContent = this.name;
-//   article.appendChild(h2);
-  
-//   var ul = document.createElement('ul');
-//   article.appendChild(ul);
   
 //   for(var i = 0; i < hours.length; i++){
 //     var li = document.createElement('li');
@@ -96,14 +91,14 @@ var renderShop = function(parentElement){
 //   ul.appendChild(li);
 // };
 
-function CookieShopLocation(name, minHourlyCustomers, maxHourlyCustomers, averageCookieSale, randomCustomerResult, cookiesSoldHourly, totalCookiesSold){
+function CookieShopLocation(name, minHourlyCustomers, maxHourlyCustomers, averageCookieSale){
   this.name = name;
   this.minHourlyCustomers = minHourlyCustomers;
   this.maxHourlyCustomers = maxHourlyCustomers;
   this.averageCookieSale = averageCookieSale;
-  this.randomCustomerResult = randomCustomerResult;
-  this.cookiesSoldHourly = cookiesSoldHourly;
-  this.totalCookiesSold = totalCookiesSold;
+  this.randomCustomerResult = [];
+  this.cookiesSoldHourly = [];
+  this.totalCookiesSold = 0;
   this.calculateRandomNumberOfCustomers = function(){
     for(var i = 0; i < hours.length; i++){
       this.randomCustomerResult.push(Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers + 1)) + this.minHourlyCustomers);
@@ -122,39 +117,16 @@ function CookieShopLocation(name, minHourlyCustomers, maxHourlyCustomers, averag
 
 CookieShopLocation.prototype.render = renderShop;
 
-// ---------------
-// Define all data to be used in script
-// ---------------
-var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-
-var shopListLocation = [];
-console.log(shopListLocation);
-
-// ---------------
-// Run script
-// ---------------
-
 var shopLocationListElement = document.getElementById('shopLocationList');
-console.log(shopLocationListElement);
 
-var oldMill = new CookieShopLocation('Old Mill', 23, 65, 6.3, [], [], 0);
-// oldMill.renderShop();
+var oldMill = new CookieShopLocation('Old Mill', 23, 65, 6.3);
 
+var pilotButte = new CookieShopLocation('Pilot Butte', 11, 38, 3.7);
 
-var pilotButte = new CookieShopLocation('Pilot Butte', 11, 38, 3.7, [], [], 0);
+var schwabAphitheater = new CookieShopLocation('Schwab Aphitheater', 20, 38, 2.3);
 
-var schwabAphitheater = new CookieShopLocation('Schwab Aphitheater', 20, 38, 2.3, [], [], 0);
+var towerTheater = new CookieShopLocation('Tower Theater', 2, 16, 4.6);
 
-var towerTheater = new CookieShopLocation('Tower Theater', 2, 16, 4.6, [], [], 0);
+var drakePark = new CookieShopLocation('Drake Park', 3, 24, 1.2);
 
-var drakePark = new CookieShopLocation('Drake Park', 3, 24, 1.2, [], [], 0);
-
-for(var i = 0; i < shopListLocation.length; i++){
-  shopListLocation[i].render(shopLocationListElement);
-} 
-
-
-
-
-
-
+oldMill.render();
