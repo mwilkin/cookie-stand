dd8f65f5d9d751eeb34ccd7757081f430294b6bf
Matt Wilkin <mwilkin22@gmail.com>
2019-03-28 14:53:57 -0700

    refactor app.js; organize in code.

--- js/app.js
+++ js/app.js
@@ -6,89 +6,117 @@ var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '
 
 var shopListLocation = [];
 var allShopsDailyTotal = [];
-// var allShopsHourlyTotal = [];
+var allShopsHourlyTotal = [];
 var companyDailyTotal = 0;
 
 // DEFINE ALL GLOBAL FUNCTIONS
+function addTableToDOM(){
+// Creating getting HTML elements
+  var parentElement = document.getElementById('shopData');
+
+  var table = document.createElement('table');
+  table.setAttribute('id', 'cookieStandSalesTable');
+  parentElement.appendChild(table);
+
+  var thead = document.createElement('thead');
+  table.appendChild(thead);
+
+  var tr = document.createElement('tr');
+  thead.appendChild(tr);
+
+  var th = document.createElement('th');
+  tr.appendChild(th);
+
+  for(var i = 0; i < hours.length; i++){
+    th = document.createElement('th');
+    th.textContent = hours[i];
+    tr.appendChild(th);
+  }
+
+  th = document.createElement('th');
+  th.textContent = 'Daily Location Total';
+  tr.appendChild(th); 
+
+  var tbody = document.createElement('tbody');
+  tbody.setAttribute('id', 'cookieStandtbody');
+  table.appendChild(tbody);
+}
+
+function populateTableWithSalesData(){
+  // var shopLocationListElement = document.getElementById('shopLocationList');
+  for(var i = 0; i < shopListLocation.length; i++){
+    shopListLocation[i].render();
+  }
+}
+
+function generateStoreData() {
+  var oldMill = new CookieShopLocation('Old Mill', 23, 65, 6.3);
+  var pilotButte = new CookieShopLocation('Pilot Butte', 11, 38, 3.7);
+  var schwabAphitheater = new CookieShopLocation('Schwab Aphitheater', 20, 38, 2.3);
+  var towerTheater = new CookieShopLocation('Tower Theater', 2, 16, 4.6);
+  var drakePark = new CookieShopLocation('Drake Park', 3, 24, 1.2);
+}
 
 // RUN CODE
-for(var i = 0; i < shopListLocation.length; i++){
-  shopListLocation[i].render(shopLocationListElement);
+
+
+function renderTable(){
+  addTableToDOM();
+  generateStoreData();
+  populateTableWithSalesData();
+  addFootertoTable();
 }
 
-// Creating getting HTML elements
+// if(parentElement){
+//  do work
 
-var parentElement = document.getElementById('shopData');
+// }
 
-var table = document.createElement('table');
-parentElement.appendChild(table);
+function addFootertoTable(){
+  var table = document.getElementById('cookieStandSalesTable');
 
-var thead = document.createElement('thead');
-table.appendChild(thead);
+  var tfoot = document.createElement('tfoot');
+  table.appendChild(tfoot);
 
-var tr = document.createElement('tr');
-thead.appendChild(tr);
+  var tr = document.createElement('tr');
+  tr.setAttribute('id', 'foot');
+  tfoot.appendChild(tr);
 
-var th = document.createElement('th');
-tr.appendChild(th);
+  var td = document.createElement('td');
+  td.textContent = 'Hourly Cookie Sale Totals';
+  tr.appendChild(td);
 
-for(var j = 0; j < hours.length; j++){
-  th = document.createElement('th');
-  th.textContent = hours[j]; 
-  tr.appendChild(th);
+  for(var i = 0; i < hours.length; i++){
+    td = document.createElement('td');
+    td.textContent = 2; // Hourly sales totals across all stores
+    tr.appendChild(td);
+  }
 }
-
-th = document.createElement('th');
-th.textContent = 'Daily Location Total';
-tr.appendChild(th); 
-
-var tbody = document.createElement('tbody');
-table.appendChild(tbody);
 
 var renderShop = function(){
-  for(i = 0; i < shopListLocation.length; i++){
-    tr = document.createElement('tr');
-    tr.setAttribute('id', 'shop'+ i);
+  var tbody = document.getElementById('cookieStandtbody');
+  console.log(shopListLocation.length);
+  for(var i = 0; i < shopListLocation.length; i++){
+    var tr = document.createElement('tr');
+    tr.setAttribute('id', 'shop' + i);
     tbody.appendChild(tr);
-    
-    var shopRow = document.getElementById('shop'+ i);
-    console.log('creating new tr with id:', 'shop'+ i);
     var td = document.createElement('td');
     td.textContent = `${shopListLocation[i].name}`;
-    shopRow.appendChild(td);
+    tr.appendChild(td);
   
-    for(var z = 0; z < hours.length; z++){
+    for(var j = 0; j < hours.length; j++){
+      console.log(hours.length);
       td = document.createElement('td');
-      td.textContent = `${shopListLocation[i].cookiesSoldHourly[z]}`;
-      shopRow.appendChild(td);
-      // allShopsHourlyTotal += parseInt(`${shopListLocation[i].cookiesSoldHourly[z]}`);
-      // console.log(allShopsHourlyTotal);
+      td.textContent = `${shopListLocation[i].cookiesSoldHourly[j]}`;
+      tr.appendChild(td);
+      // allShopsHourlyTotal.push(parseInt(`${shopListLocation[i].cookiesSoldHourly[j]}`));
     }
     td = document.createElement('td');
     td.textContent = `${shopListLocation[i].totalCookiesSold}`;
-    shopRow.appendChild(td);
+    tr.appendChild(td);
     allShopsDailyTotal.push(`${shopListLocation[i].totalCookiesSold}`);
-    console.log('an array with each shops cookie totals', `${shopListLocation[i].totalCookiesSold}`);
   }
 };
-
-var tfoot = document.createElement('tfoot');
-table.appendChild(tfoot);
-
-tr = document.createElement('tr');
-tr.setAttribute('id', 'foot');
-tfoot.appendChild(tr);
-
-var td = document.createElement('td');
-var footerData = document.getElementById('foot');
-td.textContent = 'Hourly Cookie Sale Totals';
-footerData.appendChild(td);
-
-for(i = 0; i < hours.length; i++){
-  td = document.createElement('td');
-  td.textContent = 2;
-  footerData.appendChild(td);
-}
 
 function CookieShopLocation(name, minHourlyCustomers, maxHourlyCustomers, averageCookieSale){
   this.name = name;
@@ -116,30 +144,14 @@ function CookieShopLocation(name, minHourlyCustomers, maxHourlyCustomers, averag
 
 CookieShopLocation.prototype.render = renderShop;
 
-var shopLocationListElement = document.getElementById('shopLocationList');
+renderTable(); // Entry Point
 
-var oldMill = new CookieShopLocation('Old Mill', 23, 65, 6.3);
-
-var pilotButte = new CookieShopLocation('Pilot Butte', 11, 38, 3.7);
-
-var schwabAphitheater = new CookieShopLocation('Schwab Aphitheater', 20, 38, 2.3);
-
-var towerTheater = new CookieShopLocation('Tower Theater', 2, 16, 4.6);
-
-var drakePark = new CookieShopLocation('Drake Park', 3, 24, 1.2);
-
-oldMill.render();
-
-console.log('the length of the array with all the daily cookies sales by shop', allShopsDailyTotal.length);
-
-console.log(allShopsDailyTotal);
-
-for(i = 0; i < allShopsDailyTotal.length; i++){
+for(var i = 0; i < allShopsDailyTotal.length; i++){
   companyDailyTotal += parseInt(allShopsDailyTotal[i]);
-  console.log('checking the composition of the array according to the length of the array:', allShopsDailyTotal[i]);
 }
 console.log('the running total of daily cookie sales for all shops', companyDailyTotal);
 
-td = document.createElement('td');
+var footerData = document.getElementById('foot');
+var td = document.createElement('td');
 td.textContent = companyDailyTotal;
 footerData.appendChild(td);
